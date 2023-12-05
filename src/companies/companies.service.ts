0d9802d609497160eac500e12dbd39eff438e532
Reynaldo <alex.rey.rosales.95@gmail.com>
2023-12-05 08:57:29 -0600

    Companies order by name

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,21 +1,22 @@
-import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto'; 
+import { UpdateCompanyDto } from './dto/update-company.dto';
 import { marca_empre_empresas, marca_usr_usuario } from '@prisma/client';
 import { PrismaService } from 'src/common/services/prisma.service';
 import { ApiResp } from 'src/common/class';
 
 @Injectable()
 export class CompaniesService {
-
   private readonly logger = new Logger('CompaniesService');
-  constructor(
-    private prisma: PrismaService,
-  ) { }
+  constructor(private prisma: PrismaService) {}
   async create(createCompanyDto: CreateCompanyDto, user: marca_usr_usuario) {
-
     try {
-      const userFullName = user.usr_nombres + " " + user.usr_apellidos;
+      const userFullName = user.usr_nombres + ' ' + user.usr_apellidos;
       var data: any = {
         marca_empre_usr_fk: user.marca_usr_pk,
         empre_nombre: createCompanyDto.empre_nombre,
@@ -25,46 +26,53 @@ export class CompaniesService {
         empre_contacto_telefono: createCompanyDto.empre_contacto_telefono,
         empre_usrcrea: userFullName,
         empre_usrmod: userFullName,
-      }
+      };
       const register = await this.prisma.marca_empre_empresas.create({ data });
       delete register.marca_empre_usr_fk;
       return register;
     } catch (error) {
-      this.logger.error(error)
-      if (error.code === '23505')
-        throw new BadRequestException(error.detail);
+      this.logger.error(error);
+      if (error.code === '23505') throw new BadRequestException(error.detail);
       return error;
     }
   }
 
   findAll() {
-    return this.prisma.marca_empre_empresas.findMany({ where: { empre_estado: 'ACTIVE' } });
+    return this.prisma.marca_empre_empresas.findMany({
+      where: { empre_estado: 'ACTIVE' },
+      orderBy: { empre_nombre: 'asc' },
+    });
   }
 
   async findOne(id: string) {
-    let register = await this.prisma.marca_empre_empresas.findFirst({ where: { marca_empre_pk: id, empre_estado: 'ACTIVE' } });
-    if (!register)
-      throw new NotFoundException(`Regisro no encontrado`);
+    let register = await this.prisma.marca_empre_empresas.findFirst({
+      where: { marca_empre_pk: id, empre_estado: 'ACTIVE' },
+    });
+    if (!register) throw new NotFoundException(`Regisro no encontrado`);
     return register;
   }
 
-  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: marca_usr_usuario) {
+  async update(
+    id: string,
+    updateCompanyDto: UpdateCompanyDto,
+    user: marca_usr_usuario,
+  ) {
     await this.findOne(id);
     try {
-      const userFullName = user.usr_nombres + " " + user.usr_apellidos;
+      const userFullName = user.usr_nombres + ' ' + user.usr_apellidos;
       var data: any = {
         ...updateCompanyDto,
         empre_usrmod: userFullName,
-      }
+      };
       const RespDb = await this.prisma.marca_empre_empresas.update({
         where: { marca_empre_pk: id },
-        data
+        data,
       });
       delete RespDb.marca_empre_usr_fk;
       if (!RespDb) throw new NotFoundException(`Empresa no encontrada`);
       return RespDb;
     } catch (error) {
-      this.logger.error(error)
+      this.logger.error(error);
       throw new BadRequestException(error.detail);
     }
   }
@@ -72,18 +80,18 @@ export class CompaniesService {
   async remove(id: string, user: marca_usr_usuario) {
     await this.findOne(id);
     try {
-      const userFullName = user.usr_nombres + " " + user.usr_apellidos;
+      const userFullName = user.usr_nombres + ' ' + user.usr_apellidos;
       const RespDb = await this.prisma.marca_empre_empresas.update({
         where: { marca_empre_pk: id },
         data: {
           empre_estado: 'INACTIVE',
           empre_usrmod: userFullName,
-        }
+        },
       });
       if (!RespDb) throw new NotFoundException(`Empresa no encontrada`);
       return RespDb;
     } catch (error) {
-      this.logger.error(error)
+      this.logger.error(error);
       throw new BadRequestException(error.detail);
     }
   }
